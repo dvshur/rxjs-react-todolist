4711fec5330b124086d6f2f3d7ab45e5749c06e0
Dmitry Shuranov <dvshur@gmail.com>
2017-07-21 11:37:25 +0300

    comment removed

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,21 +4,3 @@ import { Subject } from 'rxjs/Subject';
 export const addTodo$ = new Subject();
 export const toggleTodo$ = new Subject();
 export const setVisibilityFilter$ = new Subject();
-
-
-// let nextTodoId = 0
-// export const addTodo = (text) => ({
-//   type: 'ADD_TODO',
-//   id: nextTodoId++,
-//   text
-// })
-
-// export const setVisibilityFilter = (filter) => ({
-//   type: 'SET_VISIBILITY_FILTER',
-//   filter
-// })
-
-// export const toggleTodo = (id) => ({
-//   type: 'TOGGLE_TODO',
-//   id
-// })
